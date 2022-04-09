fe7343d042e57ed4fa171a12f52e9d1c1acfb5be
Rwwwrl <forgithublesh@yandex.ru>
2022-04-09 22:34:04 +0300

    feat(settings) поправил static_url, medial_url

--- main/settings.py
+++ main/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django_reinstallation_app',
     'rest_framework',
-    "debug_toolbar",
+    'debug_toolbar',
     'app',
 ]
 
@@ -122,10 +122,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static'
+STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'static'
 
-MEDIA_URL = 'media'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
